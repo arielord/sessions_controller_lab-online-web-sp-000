2d144d9aeb567c4d444c3e4482fed7db1d1dd382
Ariel <arielord@buffalo.edu>
2020-05-04 02:27:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,17 @@
 class SessionsController
   def new
-    
   end
 
   def create
-    if !params[:name]
-      redirect_to 'sessions/new'
-    end
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to controller: 'application', action: 'hello'
   end
-  
+
   def destroy
-    
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
   end
+end
 end
